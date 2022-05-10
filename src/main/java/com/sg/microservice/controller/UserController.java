86103494ed1fedf940a9e168c32dd2a364c58499
Suryo Gumilar <suryo.gumilar@gmail.com>
2022-05-10 21:44:44 +0700

    lengkap

--- src/main/java/com/sg/microservice/controller/UserController.java
+++ src/main/java/com/sg/microservice/controller/UserController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sg.microservice.entity.User;
@@ -23,5 +28,25 @@ public class UserController {
 	public ResponseEntity<List<User>>getAllUsers(){
 		List<User> allUsers = userService.getAllUsers();
 		return new ResponseEntity<List<User>>(allUsers, HttpStatus.OK);
+	}
+	
+	@GetMapping("/getbyid/{id}")
+	public ResponseEntity<User>getUserById(@PathVariable("id") int userId){
+		
+		User userById = userService.getUserById(userId);
+		return new ResponseEntity<User>(userById, HttpStatus.OK);
+	}
+	
+	@PostMapping("/addOrUpdate")
+	public ResponseEntity<User>addOrUpdate(@RequestBody User user){
+		
+		User userById = userService.addOrUpdateUser(user);
+		return new ResponseEntity<User>(userById, HttpStatus.OK);
+	}
+	@DeleteMapping("/delete")
+	public ResponseEntity<User>deleteUser(@RequestParam("id") int id) throws Exception{
+		
+		User userById = userService.deleteUser(id);
+		return new ResponseEntity<User>(userById, HttpStatus.OK);
 	}
 }
